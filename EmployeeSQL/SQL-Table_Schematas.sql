a4c1adcdd4638b44565436db5df42d9fcec57a37
Alanis R. Perez <alanisrperez@hotmail.com>
2024-07-20 16:11:22 -0700

    Updated files

--- EmployeeSQL/SQL-Table_Schematas.sql
+++ EmployeeSQL/SQL-Table_Schematas.sql
@@ -1,20 +1,16 @@
 CREATE TABLE departments(
-	dept_id INT PRIMARY KEY,
 	dept_no VARCHAR,
 	dept_name VARCHAR);
 
 CREATE TABLE dept_emp(
-	dept_id INT PRIMARY KEY,
 	emp_no INT,
 	dept_no VARCHAR);
 
 CREATE TABLE dept_manager(
-	dept_id INT PRIMARY KEY,
 	dept_no	VARCHAR,
 	emp_no INT);
 
 CREATE TABLE employees(
-	dept_id INT PRIMARY KEY,
 	emp_no INT,
 	emp_title_id VARCHAR,
 	birth_date DATE,
@@ -24,11 +20,9 @@ CREATE TABLE employees(
 	hire_date DATE);
 
 CREATE TABLE salaries(
-	dept_id INT PRIMARY KEY,
 	emp_no INT,
 	salary INT);
 
 CREATE TABLE titles(
-	dept_id INT PRIMARY KEY,
 	title_id VARCHAR,
 	title VARCHAR);
